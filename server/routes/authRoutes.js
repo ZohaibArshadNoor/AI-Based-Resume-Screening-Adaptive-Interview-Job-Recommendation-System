967b0888c7d00a4c6c0a2c338bed13460a410ae1
zohaibarshadnoor <zohaibarshadnoor@gmail.com>
2026-05-13 23:09:49 +0500

    Middleware added + JWT Authentication Completed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,14 +5,25 @@ import {
     login
 } from '../controllers/authController.js';
 
+import authMiddleware from '../middleware/authMiddleware.js';
+
 const router = express.Router();
-
-
 
 router.post('/register', register);
 
 router.post('/login', login);
 
+router.get(
+    '/me',
+    authMiddleware,
+    async (req, res) => {
 
+        res.status(200).json({
+            message: 'Protected route accessed',
+            userId: req.user.id
+        });
+
+    }
+);
 
 export default router;
